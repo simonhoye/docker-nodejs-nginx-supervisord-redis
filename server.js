7ffd62bea0d56ecc27bd83a5128580a1cd1259ec
Chris Langton <chrislangton84@gmail.com>
2017-01-15 18:11:39 +1100

    docker nginx load balanced node with redis

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ const express = require('express'),
     redis = require('redis'),
     app = express()
 
-console.log(process.env.REDIS_PORT_6379_TCP_ADDR + ':' + process.env.REDIS_PORT_6379_TCP_PORT)
-
-// Using environment variables created by Docker
 const client = redis.createClient(
-    process.env.REDIS_PORT_6379_TCP_PORT,
-    process.env.REDIS_PORT_6379_TCP_ADDR
+    process.env.REDIS_PORT,
+    'redis'
 )
 
 app.get('/', (req, res, next) =>
